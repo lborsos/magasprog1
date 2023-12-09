fc636273ab5f706b0a6e38412f1a014065461348
Borsos Laszlo <lborsos@gmail.com>
2023-12-10 01:02:50 +0200

    ZH 2023.12.09 solved 01, 02

---
+++ ZH/ZH1_20231209/F4MQFM/01/01/Program.cs
@@ -0,0 +1,111 @@
+﻿/*
+1. Feladat. 
+
+A nyári időszakban Eger egyik leglátogatottabb helyszíne az Egri Termálfürdő,
+vagy ahogyan mindenki ismeri, az Egri strand. Az Ön feladata egy olyan alkalmazás
+implementálása, mely segítségével megállapítható az egyén vagy család napi belépőjének
+ára. A belépők ára az alábbi táblázatok szerint alakul.
+
+Egyéni árak
+-----------------------------------------------
+Felnőtt belépő kedvezmények nélkül      3900 Ft
+-----------------------------------------------
+Kedvezményes jegyek
+-----------------------------------------------
+Esti belépő (du. 3 óra után)            1700 Ft
+Diák                                    1500 Ft
+Nyugdíjas, rokkantsági                  1000 Ft
+-----------------------------------------------
+
+Családi árak
+-----------------------------------------------
+2 felnőtt + 1 gyermek                   9700 Ft
+2 felnőtt + 2 gyermek                  12100 Ft
+2 felnőtt + 3 gyermek                  14500 Ft
+Minden további gyermek esetén 
+fizetendő kedvezményes ár               2400 Ft
+-----------------------------------------------
+
+A program működését az alábbiak szerint implementálja!
+
+a) Kérdezze meg a felhasználótól, milyen típusú jegyet szeretne vásárolni! ("egyéni" vagy "családi")
+
+b) Egyéni jegy esetén kérdezze meg, van-e valamilyen kedvezményre jogosultsága a felhasználónak! ("igen" vagy "nem")
+
+– Ha van, kérdezze meg mi a kedvezmény típusa és ez alapján számolja ki a jegy árát! ("diák" vagy "nyugdíjas" vagy "rokkant")
+
+– Amennyiben nincs, úgy kérdezze meg hány óra van! Oldja meg ellenőrzött
+  adatbekéréssel, hogy ez egy 9 és 18 közötti egész szám legyen, majd ennek
+  tekintetében számolja ki a belépő árát!
+
+c) Családi jegy esetén kérdezze meg a gyermekek számát! Oldja meg ellenőrzött adatbekéréssel,
+   hogy ez egy 1 és 69 közötti egész szám legyen, majd ennek tekintetében számolja ki a belépő árát!
+
+Példa: Ha a családnak 5 gyermeke van, akkor a fizetendő összeg
+14500 + 2 · 2400 = 19300 Ft
+
+d) Jelenítse meg a képernyőn a fizetendő összeget!
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // a)
+            Console.Write("A jegy tipusa (1 - egyeni; 2 - csaladi: ");
+            string jegyTipusa = Console.ReadLine();
+            string kedvezmeny = "0";
+            string kedvezmenyTipusa = "0";
+            ushort ido = 0;
+            int belepoAra = 0;
+            ushort gyerekekSzam = 0;
+            if (jegyTipusa == "1")
+            {
+                Console.Write("Van kedvezmeny? (0 - nem, 1 - igen): ");
+                kedvezmeny = Console.ReadLine();
+                if (kedvezmeny == "1")
+                {
+                    Console.Write("Van kedvezmeny tipusa? (1 - Diak, 2 - Nyugdijjas, 3 - Rokkant): ");
+                    kedvezmenyTipusa = Console.ReadLine();
+                    if (kedvezmenyTipusa == "1") belepoAra = 1500;
+                    if (kedvezmenyTipusa == "2" || kedvezmenyTipusa == "3") belepoAra = 1000;
+                }
+                else { 
+                        do
+                        {
+                            Console.Write("hany ora van? (9 - 18): ");
+                        } while (!ushort.TryParse(Console.ReadLine(), out ido) || ido < 9 || ido > 18);
+                        belepoAra = ido < 15 ? 3900 : 1700;
+                }
+            } else
+            {
+                do
+                {
+                  Console.Write("Gyerekek szama? (1 - 69): ");
+                } while (!ushort.TryParse(Console.ReadLine(), out gyerekekSzam) || gyerekekSzam < 1 || gyerekekSzam > 69);
+                if (gyerekekSzam == 1) {
+                    belepoAra = 9700;
+                } else if (gyerekekSzam == 2)
+                {
+                    belepoAra = 12100;
+                } else if (gyerekekSzam >= 3)
+                {
+                    belepoAra = 14500;
+                }
+                if (gyerekekSzam > 3) belepoAra += (gyerekekSzam - 3) * 2400;
+            }
+
+            Console.WriteLine("A belepo ara: {0}: Ft", belepoAra);
+        }
+    }
+}
---
+++ ZH/ZH1_20231209/F4MQFM/02/02/Program.cs
@@ -0,0 +1,121 @@
+﻿/*
+ 
+2. Feladat. 
+
+Egy cukorkákat forgalmazó kimérős bolt számára fejlesztünk alkalmazást. 
+A cukorkákat a felhasználó válogatja össze díszdobozba, melyek mindegyikébe 150 gramm
+termék helyezhető el. Minden egyes díszdoboz ára 110 Ft. A megvásárolható cukorkákat
+az alábbi táblázatban foglaltuk össze.
+-------------------------------------------------------------
+Megnevezések                        Tömeg           Ár
+-------------------------------------------------------------
+sima, savanyú                       10 gramm        120 Ft
+epres, narancsos, málnás, kivis     20 gramm        180 Ft
+étcsokis, tejcsokis                 30 gramm        250 Ft
+-------------------------------------------------------------
+Extra feladat: Az egyes terméktípusokat tárolja tömbökben a program könnyű bővíthetőségének
+érdekében, és az egyes feladatokat ezek segítségével oldja meg!
+
+A dobozokat nem csak teljesen megtöltve lehet megvásárolni, viszont egy dobozt mindig
+teljesen fel kell tölteni, mielőtt új dobozt nyitnánk! Ha egy cukorkát beraktunk a dobozba, már nem vehetjük ki!
+Készítsen alkalmazást, mely a cukorkák összeválogatását és a végső ár kiszámítását végzi az alábbiak szerint!
+
+a) Folyamatosan kérje be a felhasználótól, hogy milyen cukorkákat szeretne megvásárolni!
+
+b) A termékek bekérését és a fizetési szándékot a "fizetnék" kifejezés beírásával jelzi a felhasználó.
+
+c) Jelenítse meg a képernyőn, hogy a felhasználó hány dobozt kezdett meg, és mennyi a fizetendő végösszeg!
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const ushort dobozAr = 110;
+            const ushort dobozFer = 150;
+            string[] cukorkak = { "1. sima, savanyú                     10 gramm    120 Ft",
+                                  "2. epres, narancsos, málnás, kivis   20 gramm    180 Ft",
+                                  "3. étcsokis, tejcsokis               30 gramm    250 Ft" };
+            ushort[] cukorkaAr = { 120, 180, 250 };
+            ushort[] cukorkaTomeg = { 10, 20, 30 };
+            ushort[] bekertCukorka = { 0, 0, 0};
+
+            bool vege = false;
+            int kiszamoltAr = 0;
+            ushort kiszamoltDoboz = 0;
+            ushort dobozbaFerMeg = dobozFer;
+            do
+            {
+                string bekertErtek;
+                ushort kivalasztotElem;
+                Console.WriteLine("\nEddig rendeles erteke: {0} Ft, Felhasznalt doboz: {1} db; Fer meg a dobozba: {2} gramm {3}\n", kiszamoltAr, kiszamoltDoboz, dobozbaFerMeg, dobozFer);
+                cukorkakKiirasa(cukorkak);
+                do
+                {
+                    bekertErtek = Console.ReadLine();
+                    if (bekertErtek == "fizetek")
+                    {
+                        vege = true;
+                        break;
+                    }
+                    if (ushort.TryParse(bekertErtek, out kivalasztotElem) && kivalasztotElem > 0 && kivalasztotElem <= cukorkak.Length)
+                    {
+                        if (dobozbaFerMeg >= cukorkaTomeg[kivalasztotElem - 1])
+                        {
+                            bekertCukorka[kivalasztotElem - 1]++;
+                            break;
+                        } else Console.WriteLine("A kivalasztott cukorka mar nem fer be a dobozba, valassz masikot!");
+                    }
+                    Console.Write("Rosszul megadott adat!, add meg ujra: ");
+
+                } while (true);
+                kiszamoltAr = kiszamol(cukorkaAr, cukorkaTomeg, bekertCukorka, dobozAr, dobozFer, out kiszamoltDoboz, out dobozbaFerMeg);
+            } while (!vege);
+
+            Console.WriteLine("Fizetendo: {0} Ft, felhasznalt doboz: {1}", kiszamoltAr, kiszamoltDoboz);
+        }
+
+        static void cukorkakKiirasa(string[] cukorkak)
+        {
+            foreach (string item in cukorkak)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.Write("Valassz (1...{0}) (\"fizetek\" kilep): ", cukorkak.Length);
+        }
+
+        
+        static int kiszamol(ushort[] cukorkaAr, ushort[] cukorkaTomeg, ushort[] bekertCukorka, ushort dobozAr, ushort dobozFer, out ushort kiszamoltDoboz, out ushort dobozbaFerMeg)
+        {
+            int ar = 0;
+            int gramm = 0;
+            int maradek = 0;
+            for (int i = 0; i < cukorkaAr.Length; i++)
+            {
+                ar += cukorkaAr[i] * bekertCukorka[i];
+                gramm += cukorkaTomeg[i] * bekertCukorka[i];
+            }
+            kiszamoltDoboz = (ushort)(gramm / dobozFer);
+            
+            maradek = (ushort)(gramm % dobozFer);
+            if (maradek != 0)
+            {
+                dobozbaFerMeg = (ushort)(dobozFer - maradek);
+                kiszamoltDoboz++;
+            }
+            else dobozbaFerMeg = dobozFer;
+            ar += kiszamoltDoboz * dobozAr;
+            return ar;
+        }
+    }
+}
